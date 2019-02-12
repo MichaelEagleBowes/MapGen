afe9e2ef1e6a119989e6afa63d1bb2cc430598e1
ba047715 <ba047715@DESKTOP-HQRURSM.fritz.box>
2019-02-12 20:57:27 +0100

    Adjusted pom.xml for creation of runnable jar files

--- MapGenPrototype/src/main/Main.java
+++ MapGenPrototype/src/main/Main.java
@@ -1,53 +1,9 @@
 package main;
 
-import model.Model;
-import model.ModelImpl;
-import controller.MainController;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class Main {
 
-public class Main extends Application {
-
-
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-	/**
-	 * The main entry point for all JavaFX applications. The start method is called
-	 * after the init method has returned, and after the system is ready for the
-	 * application to begin running.
-	 *
-	 * <p>
-	 * NOTE: This method is called on the JavaFX Application Thread.
-	 * </p>
-	 *
-	 * @param primaryStage the primary stage for this application, onto which the
-	 *                     application scene can be set. The primary stage will be
-	 *                     embedded in the browser if the application was launched
-	 *                     as an applet. Applications may create other stages, if
-	 *                     needed, but they will not be primary stages and will not
-	 *                     be embedded in the browser.
-	 * @throws Exception if something goes wrong
-	 */
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/main.fxml"));
-		Parent root = loader.load();
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-
-		MainController controller = loader.getController();
-		//Model model = new ModelImpl();
-		controller.initialize(primaryStage, getHostServices(), controller);
-
-		//primaryStage.titleProperty().bind(model.getTitleProperty());
-		primaryStage.setMinWidth(800);
-		primaryStage.setMinHeight(500);
-		primaryStage.show();
-		root.requestFocus();
-	}
+		public static void main(String[] args) {
+			MapGen.run(args);
+		}
+	
 }
